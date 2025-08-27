6bef39e57b5241b854ea6ed08d5ff4c624032583
Lecarpentier Brian <ldqtheone@gmail.com>
2025-08-27 14:27:55 +0200

    chore(types): update SLOT and slot type for TierEditor

--- lib/slots.ts
+++ lib/slots.ts
@@ -1,1 +1,3 @@
-export const SLOTS: readonly string[] = ["Head", "Neck", "Shoulder", "Back", "Chest", "Wrist", "Hands", "Waist", "Legs", "Feet", "Ring1", "Ring2", "Trinket1", "Trinket2", "Weapon", "OffHand", "Ranged", "Tabard"] as const;
+import { Slot } from "./models";
+
+export const SLOTS: readonly Slot[] = ["Head", "Neck", "Shoulder", "Back", "Chest", "Wrist", "Hands", "Waist", "Legs", "Feet", "Ring1", "Ring2", "Trinket1", "Trinket2", "Weapon", "OffHand", "Ranged", "Tabard"] as const;
